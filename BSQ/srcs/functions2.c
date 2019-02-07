19541de463d47234f61f693803a77210c36cbe83
Manmeet2018 <35618858+Manmeet2018@users.noreply.github.com>
2019-02-06 18:54:21 -0800

    BSQ modified

---
+++ BSQ/srcs/functions2.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maparmar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/30 15:04:39 by maparmar          #+#    #+#             */
+/*   Updated: 2019/01/30 23:24:24 by bchapman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_bsq.h"
+
+void	ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+}
+
+void	ft_put_result(char *str, int start)
+{
+	int		i;
+
+	i = 0;
+	while (str[start + i])
+		i++;
+	write(1, &str[start], i);
+}
